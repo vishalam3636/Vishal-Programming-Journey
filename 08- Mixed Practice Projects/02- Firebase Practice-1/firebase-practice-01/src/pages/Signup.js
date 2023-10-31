8d10f6ca0034fe9a29ad636df11b7343f10be393
vishalam3636 <vishalam3636@gmail.com>
2023-10-31 23:20:06 +0530

    Signup using forebase

---
+++ 08- Mixed Practice Projects/02- Firebase Practice-1/firebase-practice-01/src/pages/Signup.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+import { app } from "../firebase";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+export default function Signup() {
+  const [newUser, setNewUser] = useState({ email: null, password: null });
+
+  const auth = getAuth();
+
+  const signupUser = () => {
+    createUserWithEmailAndPassword(auth, newUser.email, newUser.password).then(
+      (value) => {
+        console.log(value, ">>>> value");
+      }
+    );
+  };
+
+  const handleChange = (e) => {
+    setNewUser({ ...newUser, [e.target.name]: e.target.value });
+  };
+
+  console.log(newUser, ">>> new user");
+  return (
+    <>
+      <div className="signup-page">
+        <input
+          type="text"
+          placeholder="Enter your Email"
+          onChange={handleChange}
+          name="email"
+          required
+        />
+        <input
+          type="password"
+          placeholder="Enter your Password"
+          onChange={handleChange}
+          name="password"
+          required
+        />
+        <button onClick={signupUser}>Sign Up</button>
+      </div>
+    </>
+  );
+}
