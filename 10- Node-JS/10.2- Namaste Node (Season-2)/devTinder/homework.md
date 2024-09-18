1d1d27b50b625f2c1962df21c97d29d269d4ed7e
vishalam3636 <vishalam3636@gmail.com>
2024-09-18 22:12:40 +0530

    namaste node episode-04- Routing and Request Handlers

--- 10- Node-JS/10.2- Namaste Node (Season-2)/devTinder/homework.md
+++ 10- Node-JS/10.2- Namaste Node (Season-2)/devTinder/homework.md
@@ -17,3 +17,24 @@
   (ANS)
   Use ^ when you want to allow minor and patch updates that add features and fix bugs but avoid breaking changes in major versions.
   Use ~ when you want to allow only patch updates, focusing solely on bug fixes and avoiding new features or minor changes.
+
+<!-- HOMEWORK FROM EPISODE-04-->
+
+(part-1)
+
+- Initialize git
+- .gitIgnore
+- Create a remote repo on github
+- Push all code to remote origin
+- Play with routes and route extrensions ex. /hello , / , /hello/2 , /xyz
+- Order of the routes matters alot
+
+(Part-2)
+
+- Install Postman app and make workspace/collection and make a test API call
+- Write logic to handle GET, POST, PUT, DELETE, PATCH API calls and test them in postman
+- Explore routing and use of ?, +, (), \* in the routes
+- Use of regex in routes /a/ , /.\*fly$/
+- Reading the query params in the routes
+- Reading the query params in the routes
+- Reading the dynamic routes
--- 10- Node-JS/10.2- Namaste Node (Season-2)/devTinder/src/app.js
+++ 10- Node-JS/10.2- Namaste Node (Season-2)/devTinder/src/app.js
@@ -2,15 +2,60 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-app.use("/", (req, res) => {
-  res.send("Hello from dashboard");
-});
-+app.use("/hello", (req, res) => {
-  res.send("Hello from hello");
+/************* TEST ROUTE **************
+app.get("/hello", (req, res) => {
+  res.send("Hello Hello Hello");
 });
 
 app.use("/test", (req, res) => {
   res.send("Hello from test");
+});
+
+app.use("/", (req, res) => {
+  res.send("Hello from dashboard");
+});
+***************************************/
+
+app.use("/test", (req, res) => {
+  res.send("Hello from test");
+});
+
+app.post("/test", (req, res) => {
+  res.send("Successfully added");
+});
+
+app.patch("/test", (req, res) => {
+  res.send("Successfully updated");
+});
+
+app.delete("/test", (req, res) => {
+  res.send("Successfully deleted !!");
+});
+
+app.get("/cb", (req, res) => {
+  res.send("Hello from cb");
+});
+
+app.get("/c+b", (req, res) => {
+  res.send("Hello from c+b");
+});
+
+app.get("/c?b", (req, res) => {
+  res.send("Hello from cb");
+});
+
+app.get("/c*b", (req, res) => {
+  res.send("Hello from cb");
+});
+
+app.get("/users/:id/:name", (req, res) => {
+  console.log(req.params);
+  res.send("Hello from users");
+});
+
+app.get("/person", (req, res) => {
+  console.log(req.query);
+  res.send("Hello from person");
 });
 
 app.listen(PORT, () => {
