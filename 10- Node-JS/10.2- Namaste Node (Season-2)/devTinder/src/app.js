28895422e95363549769d6347696eaad388d2e41
vishalam3636 <vishalam3636@gmail.com>
2024-09-19 09:45:46 +0530

    episode-5_ multiple route handlers

--- 10- Node-JS/10.2- Namaste Node (Season-2)/devTinder/src/app.js
+++ 10- Node-JS/10.2- Namaste Node (Season-2)/devTinder/src/app.js
@@ -2,61 +2,32 @@ const express = require("express");
 const app = express();
 const PORT = 8000;
 
-/************* TEST ROUTE **************
-app.get("/hello", (req, res) => {
-  res.send("Hello Hello Hello");
-});
-
-app.use("/test", (req, res) => {
-  res.send("Hello from test");
-});
-
-app.use("/", (req, res) => {
-  res.send("Hello from dashboard");
-});
-***************************************/
-
-app.use("/test", (req, res) => {
-  res.send("Hello from test");
-});
-
-app.post("/test", (req, res) => {
-  res.send("Successfully added");
-});
-
-app.patch("/test", (req, res) => {
-  res.send("Successfully updated");
-});
-
-app.delete("/test", (req, res) => {
-  res.send("Successfully deleted !!");
-});
-
-app.get("/cb", (req, res) => {
-  res.send("Hello from cb");
-});
-
-app.get("/c+b", (req, res) => {
-  res.send("Hello from c+b");
-});
-
-app.get("/c?b", (req, res) => {
-  res.send("Hello from cb");
-});
-
-app.get("/c*b", (req, res) => {
-  res.send("Hello from cb");
-});
-
-app.get("/users/:id/:name", (req, res) => {
-  console.log(req.params);
-  res.send("Hello from users");
-});
-
-app.get("/person", (req, res) => {
-  console.log(req.query);
-  res.send("Hello from person");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    // Route Handler
+    console.log("Handler route user");
+    // res.send("Response !!!");
+    next();
+  },
+  (req, res, next) => {
+    // Route Handler
+    console.log("Handler route user 2");
+    // res.send("SECOND Response !!!");
+    next();
+  },
+  (req, res, next) => {
+    // Route Handler
+    console.log("Handler route user 3");
+    // res.send("THIRD Response !!!");
+    next();
+  },
+  (req, res, next) => {
+    // Route Handler
+    console.log("Handler route user 4");
+    res.send("FOURTH Response !!!");
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
