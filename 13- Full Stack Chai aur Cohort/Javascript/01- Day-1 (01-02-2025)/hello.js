62b8bf732dbd73f367ef83d2c34d95103676a74f
vishalam3636 <vishalam3636@gmail.com>
2025-02-01 21:50:09 +0530

    chai aur cohort JS- lecture-1

---
+++ 13- Full Stack Chai aur Cohort/Javascript/01- Day-1 (01-02-2025)/hello.js
@@ -0,0 +1,24 @@
+function printChai() {
+  console.log("Hello Chai !!!");
+}
+
+function bringBrush(numberOfBrush) {
+  console.log(`Haanji le aaya ${numberOfBrush} brush !!`);
+}
+
+function addTwo(num1, num2) {
+  return num1 + num2;
+}
+
+printChai();
+bringBrush(4);
+addTwo(1, 1);
+console.log(addTwo(4, 5));
+
+// Challenge-1
+function sumCake(tray1, tray2) {
+  return tray1 + tray2;
+}
+
+console.log(sumCake(12, 20));
+console.log(sumCake(12, 20));
