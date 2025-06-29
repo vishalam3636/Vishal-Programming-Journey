6212755af750e74627a7214884a99659664eb089
vishalam3636 <vishalam3636@gmail.com>
2025-06-29 23:34:08 +0530

    Complete theme color changer

--- 03- React Interview Preparation/02-context api/context-api-three-theme-change/src/App.jsx
+++ 03- React Interview Preparation/02-context api/context-api-three-theme-change/src/App.jsx
@@ -1,20 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 import ThemeBtn from "./components/ThemeBtn";
+import { ThemeProvider } from "./contexts/theme";
 
 function App() {
-  return (
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-          <Card />
-        </div>
+  let [themeMode, setThemeMode] = useState("light");
 
-        <div className="w-full max-w-sm mx-auto">
-          <ThemeBtn />
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // actual change in theme
+  useEffect(() => {
+    document.querySelector("html").classList.remove("dark", "light");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+  return (
+    <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
